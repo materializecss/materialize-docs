8e0658e34eaa73e54ad4619a2747a0019fd252db
Daniel Wurzer <wurzer.da@gmail.com>
2023-10-11 14:48:42 +0200

    fix: handlebar partials

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default {
         globSync("*.html").map((file) => [
           // // This remove the file extension from each
           // // file, so e.g. nested/foo.js becomes nested/foo
-          file.slice(4, file.length - path.extname(file).length),
-                    
+          file.slice(0, file.length - path.extname(file).length),
+
           // // This expands the relative paths to absolute paths, so e.g.
           // // src/nested/foo becomes /project/src/nested/foo.js
           fileURLToPath(new URL(file, import.meta.url)),
